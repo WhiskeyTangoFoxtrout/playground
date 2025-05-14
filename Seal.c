fcaccc0d13a6fe353d7f7f2dd48393207f237469
WhiskeyTangoFoxtrout <120198910+WhiskeyTangoFoxtrout@users.noreply.github.com>
2025-05-14 09:59:18 -0500

    Create Seal.c

---
+++ Seal.c
@@ -0,0 +1,14 @@
+//C review() and i need a real mid window()
+//also a lilfile and data transfer.  its gone be a menu and settins heres so y not()
+
+#include <stdio.h>
+#include <stdarg.h>//vari args
+#include <setjump.h>//non local jumps
+
+
+int main(){
+
+
+  
+}
+
